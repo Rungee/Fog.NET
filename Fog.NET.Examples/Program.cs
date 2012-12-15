c5c923f61758648aafa729297f477b3ff4703be0
Rungee <maksim.rylov@gmail.com>
2012-12-15 08:57:31 +0100

    update

--- Fog.NET.Examples/Program.cs
+++ Fog.NET.Examples/Program.cs
@@ -7,7 +7,7 @@ using System.Windows.Forms;
 namespace Fog.NET.Examples
 {
   static class Program
-  {
+  {   
     /// <summary>
     /// The main entry point for the application.
     /// </summary>
